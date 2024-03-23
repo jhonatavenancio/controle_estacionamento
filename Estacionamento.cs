019a11fbd3b481a117ddf547a75a8d68635c0f8e
jhonatavenancio <163940459+jhonatavenancio@users.noreply.github.com>
2024-03-22 23:43:05 -0300

    commit projeto dotnet

---
+++ Estacionamento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimulacaoFalhaTeste {
+  class Program {
+    static void Main(string[] args) {
+      // Solicite ao usuário que digite o nome do teste:
+      string nomeDoTeste = Console.ReadLine();
+
+      // Solicite ao usuário que digite a descrição do erro:
+      string descricaoDoErro = Console.ReadLine();
+
+      // Se o teste falhar, exibe a descrição do erro:
+      Console.WriteLine($"O teste falhou. Descrição: {descricaoDoErro}");
+    }
+  }
+}
